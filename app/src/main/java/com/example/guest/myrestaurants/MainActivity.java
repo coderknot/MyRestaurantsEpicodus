7ef4576fbd4c74c25f7bb9af38fc49e6a7efaf44
Chris Carr and Colin Bloom <chris+colin@>
2017-05-22 16:43:46 -0700

    commit

--- app/src/main/java/com/example/guest/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/guest/myrestaurants/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.guest.myrestaurants;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface bullyFont = Typeface.createFromAsset(getAssets(), "fonts/bully.ttf");
+        mAppNameTextView.setTypeface(bullyFont);
+
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
+            }
+        });
     }
 }
