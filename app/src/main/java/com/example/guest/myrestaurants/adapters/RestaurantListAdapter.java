efc30d69a4196f5dde405eb1a5d48043bc3b9a99
Chris Carr <coder.knot@gmail.com>
2017-05-30 12:05:17 -0700

    create custom list adapter for recycler view

---
+++ app/src/main/java/com/example/guest/myrestaurants/adapters/RestaurantListAdapter.java
@@ -0,0 +1,64 @@
+package com.example.guest.myrestaurants.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.guest.myrestaurants.R;
+import com.example.guest.myrestaurants.models.Restaurant;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder> {
+    private ArrayList<Restaurant> mRestaurants = new ArrayList<>();
+    private Context mContext;
+
+    public RestaurantListAdapter(Context context, ArrayList<Restaurant> restaurants) {
+        this.mRestaurants = restaurants;
+        this.mContext = context;
+    }
+
+    @Override
+    public RestaurantListAdapter.RestaurantViewHolder onCreateViewHolder(ViewGroup parent, int ViewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.restaurant_list_item, parent, false);
+        RestaurantViewHolder viewHolder = new RestaurantViewHolder(view);
+        return viewHolder;
+    }
+
+    public void onBindViewHolder(RestaurantListAdapter.RestaurantViewHolder holder, int position) {
+        holder.bindRestaurant(mRestaurants.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRestaurants.size();
+    }
+
+    public class RestaurantViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.restaurantImageView) ImageView mRestaurantImageView;
+        @Bind(R.id.restaurantNameTextView) TextView mNameTextView;
+        @Bind(R.id.categoryTextView) TextView mCategoryTextView;
+        @Bind(R.id.ratingTextView) TextView mRatingTextView;
+
+        private Context mContext;
+
+        public RestaurantViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindRestaurant(Restaurant restaurant) {
+            mNameTextView.setText(restaurant.getName());
+            mCategoryTextView.setText((restaurant.getCategories().get(0)));
+            mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
+        }
+    }
+}
